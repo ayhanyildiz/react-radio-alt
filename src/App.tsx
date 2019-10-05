03aa996fd3e54302c99f6bb1ef7488d11e16796d
Ayhan Yildiz <ayildiz@castleworldwide.com>
2019-10-05 13:57:20 -0400

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,70 @@ import SegmentedControl from './SegmentedControl';
 import './App.scss';
 
 const App: React.FC = () => {
-    const getData =  (data: any) => {
+    const getData = (data: any) => {
         console.log(data);
     };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Segmented Control Reusable Component</h1>
-      </header>
-      <main className="App-main">
-        <div className="App-main-samples">
-          <SegmentedControl segmentLabel="Payment Type" values={['PayPal', 'Credit Card', 'Net Term']}/>
-          <SegmentedControl segmentLabel="gender" values={['Male', 'Female']}/>
-          <SegmentedControl segmentLabel="number selection" values={['1','2','3','4']} defaultTo="3"/>
-          <SegmentedControl segmentLabel="true false selection" values={['true', 'false']}/>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Pretty Radio Box Group</h1>
+            </header>
+            <main className="App-main">
+                <div className="sg-group">
+                    <SegmentedControl
+                        segmentLabel="Payment Type"
+                        values={['PayPal', 'Credit Card', 'Net Term']}
+                    />
+                    <SegmentedControl
+                        segmentLabel="gender"
+                        values={['Male', 'Female']}
+                    />
+                    <SegmentedControl
+                        segmentLabel="number selection"
+                        values={['1', '2', '3', '4']}
+                        defaultTo="3"
+                    />
+                    <SegmentedControl
+                        segmentLabel="true false selection"
+                        values={['true', 'false']}
+                    />
+                </div>
+                <div className="sg-group">
+                    <SegmentedControl
+                        segmentLabel="Payment Type"
+                        values={['PayPal', 'Credit Card', 'Net Term']}
+                    />
+                    <SegmentedControl
+                        segmentLabel="gender"
+                        values={['Male', 'Female']}
+                    />
+                    <SegmentedControl
+                        segmentLabel="number selection"
+                        values={['1', '2', '3', '4']}
+                        defaultTo="3"
+                    />
+                </div>
+                <div className="sg-group">
+                    <SegmentedControl
+                        segmentLabel="Payment Type"
+                        values={['PayPal', 'Credit Card', 'Net Term']}
+                    />
+                    <SegmentedControl
+                        segmentLabel="gender"
+                        values={['Male', 'Female']}
+                    />
+                </div>
+                <div className="sg-group">
+                    <SegmentedControl
+                        sendData={getData}
+                        segmentLabel="Payment Type"
+                        values={['PayPal', 'Credit Card', 'Net Term']}
+                    />
+                </div>
+            </main>
         </div>
-      <div className="App-main-samples">
-          <SegmentedControl segmentLabel="Payment Type" values={['PayPal', 'Credit Card', 'Net Term']}/>
-          <SegmentedControl segmentLabel="gender" values={['Male', 'Female']}/>
-          <SegmentedControl segmentLabel="number selection" values={['1','2','3','4']} defaultTo="3"/>
-      </div>
-      <div className="App-main-samples">
-          <SegmentedControl segmentLabel="Payment Type" values={['PayPal', 'Credit Card', 'Net Term']}/>
-          <SegmentedControl segmentLabel="gender" values={['Male', 'Female']}/>
-      </div>
-      <div className="App-main-samples">
-          <SegmentedControl sendData={getData} segmentLabel="Payment Type" values={['PayPal', 'Credit Card', 'Net Term']}/>
-      </div>
-      </main>
-    </div>
-  );
+    );
 };
 
 export default App;
